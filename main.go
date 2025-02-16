96d6031d43a512e6d4f8dc911d5f76be5edf1b80
dyhalmeida <dyhalmeida@gmail.com>
2025-02-16 08:17:07 -0300

    simple concurrency problem

--- main.go
+++ main.go
@@ -1,58 +1,41 @@
+/*
+*
+Esse código apresenta um problema de concorrência quando múltiplas requisições HTTP são feitas simultaneamente.
+
+O Problema de Concorrência
+A variável count é uma variável global e está sendo modificada diretamente dentro do handler HTTP,
+sem nenhuma forma de sincronização.
+
+O que acontece quando múltiplas requisições chegam ao mesmo tempo?
+
+Como o servidor HTTP do Go lida com cada requisição em uma goroutine separada,
+várias goroutines podem tentar atualizar count ao mesmo tempo. Isso leva a uma condição de corrida (race condition),
+onde duas ou mais goroutines podem ler e escrever a variável simultaneamente, resultando em um valor inconsistente de count.
+
+Exemplo do problema:
+Duas requisições chegam quase ao mesmo tempo.
+Ambas as goroutines leem o valor atual de count (suponha que seja 10).
+Ambas calculam count + 1 e atribuem o novo valor (11).
+O valor de count deveria ser 12, mas como ambas escreveram 11, perdemos um incremento.
+*
+*/
 package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
 )
 
-func Task(name string) {
-	for i := 1; i < 11; i++ {
-		fmt.Printf("%d - Task %s is running\n", i, name)
-		time.Sleep(1 * time.Second)
-	}
-}
-
-func SerialProcessing() {
-	Task("A")
-	Task("B")
-}
-
-// SimultaneousProcessing is thread 1
-func SimultaneousProcessing() {
-
-	// Task C is thread 2
-	go Task("C")
-
-	// Task D is thread 3
-	go Task("D")
-
-	// Task anonymous E is thread 4
-	go func(name string) {
-		for i := 1; i < 11; i++ {
-			fmt.Printf("%d - Task anonymous %s is running\n", i, name)
-			time.Sleep(1 * time.Second)
-		}
-	}("E")
-
-	// temporary solution to arrest Simultaneous Processing
-	time.Sleep(10 * time.Second)
-}
+var count int64 = 0
 
 func main() {
-	fmt.Println("SerialProcessing start")
-	fmt.Println("")
 
-	SerialProcessing()
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		count = count + 1
+		res.WriteHeader(http.StatusOK)
+		res.Write([]byte(fmt.Sprintf("Você é o visitante número: %d", count)))
 
-	fmt.Println("")
-	fmt.Println("SerialProcessing finish")
+	})
 
-	fmt.Println("")
-	fmt.Println("SimultaneousProcessing start")
-	fmt.Println("")
-
-	SimultaneousProcessing()
-
-	fmt.Println("")
-	fmt.Println("SimultaneousProcessing finish")
+	http.ListenAndServe(":8080", nil)
 }
