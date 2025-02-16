c79c59b7aea0b39134b6eacf437d4f5b1f053be5
dyhalmeida <dyhalmeida@gmail.com>
2025-02-16 08:46:24 -0300

    concurrency problem with mutex solution

--- main.go
+++ main.go
@@ -1,43 +1,51 @@
 /*
-Esse código apresenta um problema de concorrência quando múltiplas requisições HTTP são feitas simultaneamente.
+# O que é o problema de concorrência nesse caso?
+No código original, várias goroutines podem acessar e modificar count ao mesmo tempo,
+causando uma condição de corrida. Isso resulta em valores inconsistentes.
 
-O Problema de Concorrência
-A variável count é uma variável global e está sendo modificada diretamente dentro do handler HTTP,
-sem nenhuma forma de sincronização.
+# Como o código resolve isso?
+A solução está no uso do mutex (sync.Mutex), que funciona como um cadeado para garantir
+que apenas uma goroutine por vez possa modificar count.
 
-O que acontece quando múltiplas requisições chegam ao mesmo tempo?
+- Criamos um mutex (m), que será usado para controlar o acesso à variável count.
+- m.Lock() trava o acesso à variável count. Enquanto uma goroutine estiver executando esse trecho, outras terão que esperar.
+- Incrementa count com segurança.
+- m.Unlock() libera o acesso para que outra goroutine possa modificar count.
 
-Como o servidor HTTP do Go lida com cada requisição em uma goroutine separada,
-várias goroutines podem tentar atualizar count ao mesmo tempo. Isso leva a uma condição de corrida (race condition),
-onde duas ou mais goroutines podem ler e escrever a variável simultaneamente, resultando em um valor inconsistente de count.
+# Como o Mutex resolve o problema?
+- Antes de modificar count, uma goroutine trava o mutex (m.Lock()).
+- Enquanto o mutex está travado, nenhuma outra goroutine pode modificar count.
+- Depois de atualizar count, o mutex é destravado (m.Unlock()), permitindo que a próxima goroutine continue.
+- Assim, cada requisição manipula count de forma segura e ordenada.
 
-Exemplo do problema:
-Duas requisições chegam quase ao mesmo tempo.
-Ambas as goroutines leem o valor atual de count (suponha que seja 10).
-Ambas calculam count + 1 e atribuem o novo valor (11).
-O valor de count deveria ser 12, mas como ambas escreveram 11, perdemos um incremento.
-
-Para o teste foi utilizado o comando do apache benchmark (ab):
+# Para o teste foi utilizado o comando do apache benchmark (ab):
 
 ab -n 10000 -c 100 http://localhost:8080/
 
 -n 10000 -> Número total de requisições que serão enviadas (10.000 requisições).
 -c 100 -> Número de conexões concorrentes, ou seja, quantas requisições serão feitas ao mesmo tempo (100 requisições simultâneas).
 http://localhost:8080/ -> URL do servidor que será testado (localhost na porta 8080)
+
+# Para detectar race condition em modo de desenvolvimento, foi utilizando o parâmetro -race no comando go run
+
+go run -race main.go
 */
 package main
 
 import (
 	"fmt"
 	"net/http"
+	"sync"
 )
 
 var count int64 = 0
 
 func main() {
-
+	m := sync.Mutex{}
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		m.Lock()
 		count = count + 1
+		m.Unlock()
 		res.WriteHeader(http.StatusOK)
 		res.Write([]byte(fmt.Sprintf("Você é o visitante número: %d", count)))
 
