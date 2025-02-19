81f069917d2b4039f7718cc694b0d99f2d99025a
dyhalmeida <dyhalmeida@gmail.com>
2025-02-19 18:41:24 -0300

    channel with range

--- main.go
+++ main.go
@@ -1,58 +1,44 @@
+/*
+# O que é o problema de concorrência neste caso?
+O problema de concorrência no código é a interação entre duas goroutines:
+a goroutine principal (main()) que está lendo do canal e a goroutine publish que está escrevendo no canal.
+Em sistemas concorrentes, o problema de concorrência ocorre quando múltiplas goroutines tentam acessar
+e modificar o mesmo recurso (neste caso, o canal) ao mesmo tempo de forma descoordenada,
+podendo levar a resultados inconsistentes ou bloqueios (deadlock).
+
+# Como o código resolve isso?
+O código utiliza goroutines e canais para coordenar a comunicação entre elas de forma eficiente e segura
+
+# Controle de fluxo com goroutines:
+- A goroutine main aguarda os dados enviados pelo canal e imprime cada valor.
+- A goroutine publish envia valores para o canal e, uma vez que todos os dados são enviados, fecha o canal.
+Isso sinaliza para a goroutine main que não haverá mais dados.
+
+# Uso do range no canal
+No código, o loop for ch := range channel da goroutine main é uma maneira de consumir os dados do canal até que o canal seja fechado.
+O range em Go cuida automaticamente da sincronização, interrompendo a iteração quando o canal é fechado e não há mais dados.
+
+# Evitando de Deadlock
+Deadlock ocorre quando duas ou mais goroutines ficam esperando uma pela outra indefinidamente, sem que nenhuma delas consiga prosseguir, causando um bloqueio no programa.
+No caso deste código, o deadlock seria um problema se a goroutine principal ficasse esperando para ler do canal enquanto a goroutine publish nunca escrevesse nada ou não fechasse o canal.
+A goroutine publish envia números para o canal de 0 a 10 e, em seguida, fecha o canal.
+O fechamento do canal é crucial. Ele informa à goroutine principal que todos os dados foram enviados e que não há mais dados a serem lidos, permitindo que o loop range na goroutine principal termine de maneira segura.
+*/
+
 package main
 
-import (
-	"fmt"
-	"time"
-)
+func main() {
+	channel := make(chan int)
+	go publish(channel)
 
-func Task(name string) {
-	for i := 1; i < 11; i++ {
-		fmt.Printf("%d - Task %s is running\n", i, name)
-		time.Sleep(1 * time.Second)
+	for ch := range channel {
+		println(ch)
 	}
 }
 
-func SerialProcessing() {
-	Task("A")
-	Task("B")
-}
-
-// SimultaneousProcessing is thread 1
-func SimultaneousProcessing() {
-
-	// Task C is thread 2
-	go Task("C")
-
-	// Task D is thread 3
-	go Task("D")
-
-	// Task anonymous E is thread 4
-	go func(name string) {
-		for i := 1; i < 11; i++ {
-			fmt.Printf("%d - Task anonymous %s is running\n", i, name)
-			time.Sleep(1 * time.Second)
-		}
-	}("E")
-
-	// temporary solution to arrest Simultaneous Processing
-	time.Sleep(10 * time.Second)
-}
-
-func main() {
-	fmt.Println("SerialProcessing start")
-	fmt.Println("")
-
-	SerialProcessing()
-
-	fmt.Println("")
-	fmt.Println("SerialProcessing finish")
-
-	fmt.Println("")
-	fmt.Println("SimultaneousProcessing start")
-	fmt.Println("")
-
-	SimultaneousProcessing()
-
-	fmt.Println("")
-	fmt.Println("SimultaneousProcessing finish")
+func publish(channel chan int) {
+	for i := 0; i <= 10; i++ {
+		channel <- i
+	}
+	close(channel)
 }
